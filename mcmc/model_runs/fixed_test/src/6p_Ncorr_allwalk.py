2adb398ad5925b5a0cca62cf7c5ee160844c40b3
Sebastian Monzon <jsmonzon@ucsc.edu>
2023-11-29 16:25:39 -0500

    new version of the mcmc hammer

---
+++ mcmc/model_runs/fixed_test/src/6p_Ncorr_allwalk.py
@@ -0,0 +1,83 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pygtc
+    
+import sys 
+sys.path.insert(0, '/Users/jsmonzon/Research/SatGen/mcmc/src/')
+import jsm_SHMR
+import jsm_mcmc
+
+print("Setting up the run")
+
+chain_name = "6p_Ncorr_allwalk/"
+
+fid_theta = [1.9, 0.3, 10.5, 0, 0, 0]
+guess_theta = [2, 0.2, 10.4, -0.1, -0.01, 0]
+priors = [[-1,7], [0,5], [10,11], [-3,2], [-2,2], [-2,2]]
+params = ["$\\alpha$", "$\\sigma_0$", "$M_{*}$", "$\\delta$", "$\\beta$", "$\\gamma$"]
+fixed = [False, False, True, True, True, True]
+
+ndim = len(fid_theta)
+nfixed = sum(fixed)
+N_corr = True
+p0_corr = False
+
+a_stretch = 2.0
+nwalk = 50
+nstep = 100
+ncores = 8
+min_mass = 6.5
+
+savedir = "/Users/jsmonzon/Research/SatGen/mcmc/model_runs/fixed_test/"+chain_name
+savefile = savedir+"chain.h5"
+
+hammer = jsm_mcmc.Hammer(ftheta=fid_theta, gtheta=guess_theta, fixed=fixed, ndim=ndim, nwalk=nwalk, nstep=nstep, ncores=ncores,
+                         a_stretch=a_stretch, min_mass=min_mass, N_corr=N_corr, p0_corr=p0_corr, savedir=savedir, savefile=savefile)
+
+print("reading in the data")
+massdir = "/Users/jsmonzon/Research/data/MW-analog/meta_data_psi3/"
+data = jsm_mcmc.init_data(fid_theta, "/Users/jsmonzon/Research/SatGen/mcmc/model_runs/fixed_test/mock_data.npy")
+data.get_stats(min_mass=min_mass, plot=False)
+data.get_data_points(plot=False)
+
+print("defining the forward model")
+models = jsm_mcmc.load_models(massdir, read_red=True) # need to change this for every run!
+
+def lnprior(theta):
+    if priors[0][0] < theta[0] < priors[0][1] and\
+        priors[1][0] < theta[1] < priors[1][1] and\
+         priors[2][0] < theta[2] < priors[2][1] and\
+          priors[3][0] < theta[3] < priors[3][1] and\
+           priors[4][0] < theta[4] < priors[4][1] and\
+            priors[5][0] < theta[5] < priors[5][1]:
+        lp = 0.0
+    else:
+        lp = -np.inf
+    return lp
+
+def forward(theta):
+    fixed_theta = np.where(fixed, fid_theta, theta).tolist()
+    models.convert_zacc(fixed_theta, jsm_SHMR.redshift)
+    models.get_stats(min_mass=min_mass)
+    return models.stat.Pnsat, models.stat.Msmax, models.stat.ecdf_MsMax
+
+def lnlike(theta):
+    model_Pnsat, models_Msmax, _ = forward(theta)
+    lnL_sat = jsm_mcmc.lnL_Pnsat(model_Pnsat, data.stat.satfreq)
+    lnL_max = jsm_mcmc.lnL_KS(models_Msmax, data.stat.Msmax)
+    return lnL_sat + lnL_max
+
+def lnprob(theta):
+    lp = lnprior(theta)
+    if not np.isfinite(lp):
+        return -np.inf
+    else:
+        return lp + lnlike(theta)
+    
+print("running the mcmc!")
+
+hammer.runit(lnprob)
+hammer.write_output()
+
+    
--- mcmc/src/jsm_mcmc.py
+++ mcmc/src/jsm_mcmc.py
@@ -15,22 +15,20 @@ import warnings; warnings.simplefilter('ignore')
 ###    A SIMPLE FUNC TO MULTICORE THE MCMC     ###
 ##################################################
 
-def RUN(theta_0, lnprob, nwalkers, niter, ndim, ncores, a_stretch):
+# def RUN_old(theta_0, lnprob, nwalkers, niter, ndim, ncores, a_stretch, nfixed):
 
-    p0 = [np.array(theta_0) + 1e-2 * np.random.randn(ndim) for i in range(nwalkers)]
+#     p0 = [np.array(theta_0) + 1e-2 * np.random.randn(ndim) for i in range(nwalkers)]
     
-    with Pool(ncores) as pool:
-        sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, pool=pool, moves=emcee.moves.StretchMove(a=a_stretch))
-        start = time.time()
-        sampler.run_mcmc(p0, niter)
-        end = time.time()
-        multi_time = end - start
-        print("Run took {0:.1f} seconds".format(multi_time))
-        print("Run took {0:.1f} minutes".format(multi_time/60))
-        print("Run took {0:.1f} hours".format(multi_time/3600))
-
-
-    return sampler
+#     with Pool(ncores) as pool:
+#         sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, pool=pool, moves=emcee.moves.StretchMove(a=a_stretch, nf=nfixed))
+#         start = time.time()
+#         sampler.run_mcmc(p0, niter)
+#         end = time.time()
+#         multi_time = end - start
+#         print("Run took {0:.1f} seconds".format(multi_time))
+#         print("Run took {0:.1f} minutes".format(multi_time/60))
+#         print("Run took {0:.1f} hours".format(multi_time/3600))
+#     return sampler
 
 def lnL_Pnsat(model, data):
     lnL = np.sum(np.log(model[data]))
@@ -136,22 +134,86 @@ class load_models:
         self.stat.Maxmass()
         
 
-##################################################
-###     TO INTERFACE WITH THE MCMC OUTPUT      ###
-##################################################
+class Hammer:
 
-class inspect_run:
+    def __init__(self, ftheta, gtheta, fixed, ndim, nwalk, nstep, ncores, a_stretch, min_mass, N_corr=True, p0_corr=False, **kwargs):
 
-    def __init__(self, sampler, **kwargs):
+        self.ftheta = ftheta
+        self.gtheta = gtheta
+        self.fixed = fixed
+        self.ndim = ndim
+        self.nwalk = nwalk
+        self.nstep = nstep
+        self.ncores = ncores
+        self.a_stretch = a_stretch
+        self.min_mass = min_mass
+        self.Ncore = N_corr
+        self.p0_corr = p0_corr
+
+        if N_corr==True:
+            print("making the correction on Ndim in the stretch move algorithm!")
+            self.nfixed = sum(self.fixed)
+        else:
+            self.nfixed = 0
+
         for key, value in kwargs.items():
             setattr(self, key, value)
+
+        self.inital_guess()
+        self.write_init()
+
+    def inital_guess(self):
+        p0 = [np.array(self.gtheta) + 1e-2 * np.random.randn(self.ndim) for i in range(self.nwalk)]
+
+        if self.p0_corr:
+            print("not allowing the fixed walkers to step!")
+            p0_fixed = []
+            for i in range(self.nwalk):
+                p0_fixed.append(np.where(self.fixed, self.ftheta, p0[i]))
+            self.p0 = p0_fixed
+
+        else:
+            print("allowing the fixed walkers to step! make sure the likelyhood evaluation is correct!")
+            self.p0 = p0
+
+    def write_init(self):
+        with open(self.savedir+"chain_info.txt", 'w') as file: 
+            
+            write = ['This run was measured against data with truth values of '+str(self.ftheta)+'\n', 
+            'It was initialized at '+str(self.gtheta)+'\n', 
+            'The chain has '+str(self.nwalk)+' walkers and '+str(self.nstep)+' steps\n', 
+            'It was initialized with a_stretch = '+str(self.a_stretch)+'\n']
+            file.writelines("% s\n" % line for line in write) 
+            file.close()
+
+    def runit(self, lnprob):
+        backend = emcee.backends.HDFBackend(self.savefile)
+        backend.reset(self.nwalk, self.ndim)
         
-        self.ndim = len(self.start_theta)
+        with Pool(self.ncores) as pool:
+            sampler = emcee.EnsembleSampler(self.nwalk, self.ndim, lnprob, pool=pool, moves=emcee.moves.StretchMove(a=self.a_stretch, nf=self.nfixed), backend=backend)
+            start = time.time()
+            sampler.run_mcmc(self.p0, self.nstep, progress=True, skip_initial_state_check=self.p0_corr)
+            end = time.time()
+            multi_time = end - start
+        
+        print("Run took {0:.1f} hours".format(multi_time/3600))
+        print("saving some information from the sampler class")
+
+        self.runtime = multi_time/3600
+        self.acceptance_frac = np.mean(sampler.acceptance_fraction)
+        try:
+            self.tau = sampler.get_autocorr_time()
+        except:
+            print("run a longer chain!")
+            self.tau = 0
+
         self.samples = sampler.get_chain()
         self.chisq = sampler.get_log_prob()*(-2)
         self.last_samp = sampler.get_last_sample().coords
         self.last_chisq = sampler.get_last_sample().log_prob*(-2)
-        self.acceptance_frac = np.mean(sampler.acceptance_fraction)
+
+    def write_output(self):
 
         values = []
         for i in range(self.ndim):
@@ -160,27 +222,11 @@ class inspect_run:
             values.append([post[1], q[0], q[1]])
         self.constraints = values
 
-        print("saving the chain!")
-        self.write_init()
-        self.save_sample()
-
-        print("making some figures")
-        self.full_chain()
-        self.chisq_last_sample()
-        self.corner_last_sample()
-        self.stat_fit()
-        self.SHMR_last_sample() # for the sigma value!
-
-    def write_init(self):
-        with open(self.savedir+"chain_initialization.txt", 'w') as file: 
-            
-            write1 = ['This run was measured against data with truth values of '+str(self.data.truths)+'\n', 
-            'It was initialized at '+str(self.start_theta)+'\n', 
-            'The chain has '+str(self.samples.shape[1])+' walkers and '+str(self.samples.shape[0])+' steps\n', 
-            'It was initialized with a_stretch = '+str(self.a_stretch)+'\n', 
-            'The mean acceptance fraction is '+str(self.acceptance_frac)+'\n', 
-            'The final step in the chain gives the following constraints\n']
-
+        with open(self.savedir+"chain_info.txt", 'a') as file:
+            write1 = ['The run took {0:.1f} hours'.format(self.runtime)+'\n',
+                      'The mean acceptance fraction turned out to be '+str(self.acceptance_frac)+'\n',
+                      'The auto correlation time (Nsteps) was' + str(self.tau)+'\n',
+                      'The final step in the chain gives the following constraints on theta\n']
             write2 = []
             for i,val in enumerate(self.constraints):
                 write2.append(self.labels[i]+"="+str(val)+"\n")      
@@ -189,12 +235,10 @@ class inspect_run:
             file.writelines("% s\n" % line for line in write2) 
             file.close()
 
-    def save_sample(self):
-        np.savez(self.savedir+"samples.npz", 
-                 coords = self.samples,
-                 chisq = self.chisq)
-        
-    def full_chain(self):
+        self.plot_chain()
+        self.plot_last_chisq()
+
+    def plot_chain(self):
         if self.samples.shape[1] > 1000:
             a = 0.01
         else:
@@ -209,16 +253,11 @@ class inspect_run:
             ax.yaxis.set_label_coords(-0.1, 0.5)
 
         axes[-1].set_xlabel("step number")
-        plt.savefig(self.savedir+"chain.png")
+        if self.savefig == True:
+            plt.savefig(self.savedir+"chain.png")
 
-    def corner_last_sample(self, zoom=True):        
-        if zoom==True:
-            fig = corner.corner(self.last_samp, show_titles=True, labels=self.labels, truths=self.data.truths, quantiles=[0.15, 0.5, 0.85], plot_datapoints=False)
-        elif zoom==False:
-            fig = corner.corner(self.last_samp, show_titles=True, labels=self.labels, truths=self.data.truths, range=self.priors , quantiles=[0.15, 0.5, 0.85], plot_datapoints=False)
-        plt.savefig(self.savedir+"corner.png")
 
-    def chisq_last_sample(self):
+    def plot_last_chisq(self):
         fig, axs = plt.subplots(1, self.ndim, sharey=True, figsize=(14,8))
         fig.tight_layout(pad=2.0)
 
@@ -228,7 +267,9 @@ class inspect_run:
             axs[i].axvline(self.data.truths[i], ls=":", color="black")
         axs[0].set_ylabel("$\\chi^2$", fontsize=12)
 
-        plt.savefig(self.savedir+"chi2_final.png")
+        if self.savefig == True:
+            plt.savefig(self.savedir+"chi2_final.png")
+
 
     def stat_fit(self):
 
@@ -258,13 +299,13 @@ class inspect_run:
         plt.plot(self.data.stat.Msmax, self.data.stat.ecdf_MsMax, color="black")
         plt.xlabel("stellar mass of most massive satellite ($\mathrm{log\ M_{\odot}}$)", fontsize=15)
         plt.ylabel("CDF", fontsize=15)
-        plt.savefig(self.savedir+"S2.png")
+
+        if self.savefig == True:
+            plt.savefig(self.savedir+"S2.png")
+
 
     def SHMR_last_sample(self):
 
-        # plot_data = np.load("../plot_data.npy")
-        # self.halo_masses = plot_data[0]
-        # self.redshifts = plot_data[1]
         self.halo_masses = np.linspace(6,12,50)
         SHMR_mat = np.zeros(shape=(self.last_samp.shape[0], self.halo_masses.shape[0]))
 
@@ -308,7 +349,185 @@ class inspect_run:
         plt.ylabel("M$_{*}$ (M$_\odot$)", fontsize=15)
         plt.xlabel("M$_{\mathrm{vir}}$ (M$_\odot$)", fontsize=15)
         plt.legend(fontsize=12)
-        plt.savefig(self.savedir+"SHMR.png")
+
+        if self.savefig == True:
+            plt.savefig(self.savedir+"SHMR.png")
+
+
+
+##################################################
+###     TO INTERFACE WITH THE MCMC OUTPUT      ###
+##################################################
+
+# class inspect_run:
+
+#     def __init__(self, sampler, **kwargs):
+#         for key, value in kwargs.items():
+#             setattr(self, key, value)
+        
+#         self.ndim = len(self.start_theta)
+#         self.samples = sampler.get_chain()
+#         self.chisq = sampler.get_log_prob()*(-2)
+#         self.last_samp = sampler.get_last_sample().coords
+#         self.last_chisq = sampler.get_last_sample().log_prob*(-2)
+#         self.acceptance_frac = np.mean(sampler.acceptance_fraction)
+
+#         values = []
+#         for i in range(self.ndim):
+#             post = np.percentile(self.last_samp[:, i], [16, 50, 84])
+#             q = np.diff(post)
+#             values.append([post[1], q[0], q[1]])
+#         self.constraints = values
+
+#         print("saving the chain!")
+#         self.write_init()
+#         self.save_sample()
+
+#         print("making some figures")
+#         self.full_chain()
+#         self.chisq_last_sample()
+#         self.corner_last_sample()
+#         self.stat_fit()
+#         self.SHMR_last_sample() # for the sigma value!
+
+#     def write_init(self):
+#         with open(self.savedir+"chain_initialization.txt", 'w') as file: 
+            
+#             write1 = ['This run was measured against data with truth values of '+str(self.data.truths)+'\n', 
+#             'It was initialized at '+str(self.start_theta)+'\n', 
+#             'The chain has '+str(self.samples.shape[1])+' walkers and '+str(self.samples.shape[0])+' steps\n', 
+#             'It was initialized with a_stretch = '+str(self.a_stretch)+'\n', 
+#             'The mean acceptance fraction is '+str(self.acceptance_frac)+'\n', 
+#             'The final step in the chain gives the following constraints\n']
+
+#             write2 = []
+#             for i,val in enumerate(self.constraints):
+#                 write2.append(self.labels[i]+"="+str(val)+"\n")      
+
+#             file.writelines("% s\n" % line for line in write1) 
+#             file.writelines("% s\n" % line for line in write2) 
+#             file.close()
+
+#     def save_sample(self):
+#         np.savez(self.savedir+"samples.npz", 
+#                  coords = self.samples,
+#                  chisq = self.chisq)
+        
+#     def full_chain(self):
+#         if self.samples.shape[1] > 1000:
+#             a = 0.01
+#         else:
+#             a = 0.1
+
+#         fig, axes = plt.subplots(self.ndim, figsize=(10, 7), sharex=True)
+#         for i in range(self.ndim):
+#             ax = axes[i]
+#             ax.plot(self.samples[:, :, i], "k", alpha=a)
+#             ax.set_xlim(0, len(self.samples))
+#             ax.set_ylabel(self.labels[i])
+#             ax.yaxis.set_label_coords(-0.1, 0.5)
+
+#         axes[-1].set_xlabel("step number")
+#         plt.savefig(self.savedir+"chain.png")
+
+#     def corner_last_sample(self, zoom=True):        
+#         if zoom==True:
+#             fig = corner.corner(self.last_samp, show_titles=True, labels=self.labels, truths=self.data.truths, quantiles=[0.15, 0.5, 0.85], plot_datapoints=False)
+#         elif zoom==False:
+#             fig = corner.corner(self.last_samp, show_titles=True, labels=self.labels, truths=self.data.truths, range=self.priors , quantiles=[0.15, 0.5, 0.85], plot_datapoints=False)
+#         plt.savefig(self.savedir+"corner.png")
+
+#     def chisq_last_sample(self):
+#         fig, axs = plt.subplots(1, self.ndim, sharey=True, figsize=(14,8))
+#         fig.tight_layout(pad=2.0)
+
+#         for i in range(self.ndim):
+#             axs[i].scatter(self.last_samp[:,i], self.last_chisq, marker=".")
+#             axs[i].set_xlabel(self.labels[i], fontsize=12)
+#             axs[i].axvline(self.data.truths[i], ls=":", color="black")
+#         axs[0].set_ylabel("$\\chi^2$", fontsize=12)
+
+#         plt.savefig(self.savedir+"chi2_final.png")
+
+#     def stat_fit(self):
+
+#         Ns, Ms, _ = self.forward(self.last_samp[0])
+#         Pnsat_mat = np.zeros(shape=(self.last_samp.shape[0], Ns.shape[0]))
+#         Msmax_mat = np.zeros(shape=(self.last_samp.shape[0], Ms.shape[0]))
+#         Msmaxe_mat = np.zeros(shape=(self.last_samp.shape[0], Ms.shape[0]))
+
+#         for i, theta in enumerate(self.last_samp):
+#             tPnsat, tMsmax, tecdf_MsMax = self.forward(theta)
+#             Pnsat_mat[i] = tPnsat
+#             Msmax_mat[i] = tMsmax      
+#             Msmaxe_mat[i] = tecdf_MsMax
+
+#         plt.figure(figsize=(8, 8))
+#         for i in Pnsat_mat:
+#             plt.plot(np.arange(i.shape[0]),i, color="grey", alpha=0.1)
+#         plt.plot(np.arange(self.data.stat.Pnsat.shape[0]),self.data.stat.Pnsat,marker="o", color="black")
+#         plt.xlabel("number of satellites > $10^{"+str(6.5)+"} \mathrm{M_{\odot}}$", fontsize=15)
+#         plt.ylabel("PDF", fontsize=15)
+#         plt.xlim(0,25)
+#         plt.savefig(self.savedir+"S1.png")
+
+#         plt.figure(figsize=(8, 8))
+#         for i, val in enumerate(Msmax_mat):
+#             plt.plot(val, Msmaxe_mat[i], color="grey", alpha=0.1)
+#         plt.plot(self.data.stat.Msmax, self.data.stat.ecdf_MsMax, color="black")
+#         plt.xlabel("stellar mass of most massive satellite ($\mathrm{log\ M_{\odot}}$)", fontsize=15)
+#         plt.ylabel("CDF", fontsize=15)
+#         plt.savefig(self.savedir+"S2.png")
+
+#     def SHMR_last_sample(self):
+
+#         # plot_data = np.load("../plot_data.npy")
+#         # self.halo_masses = plot_data[0]
+#         # self.redshifts = plot_data[1]
+#         self.halo_masses = np.linspace(6,12,50)
+#         SHMR_mat = np.zeros(shape=(self.last_samp.shape[0], self.halo_masses.shape[0]))
+
+#         if self.SHMR_model == "simple":
+#             self.fid_Ms = jsm_SHMR.simple([self.data.truths[0], 0], self.halo_masses)
+#             for i,val in enumerate(self.last_samp):  # now pushing all thetas through!
+#                 SHMR_mat[i] = jsm_SHMR.simple([val[0], 0], self.halo_masses)
+
+#         elif self.SHMR_model =="anchor":
+#             self.fid_Ms = jsm_SHMR.anchor([self.data.truths[0], 0, self.data.truths[2]], self.halo_masses)
+#             for i,val in enumerate(self.last_samp):  # now pushing all thetas through!
+#                 SHMR_mat[i] = jsm_SHMR.anchor([val[0], 0, val[2]], self.halo_masses)
+
+#         elif self.SHMR_model =="curve":
+#             self.fid_Ms = jsm_SHMR.curve([self.data.truths[0], 0, self.data.truths[2],  self.data.truths[3]], self.halo_masses)
+#             for i,val in enumerate(self.last_samp):  # now pushing all thetas through!
+#                 SHMR_mat[i] = jsm_SHMR.curve([val[0], 0, val[2], val[3]], self.halo_masses)
+
+#         elif self.SHMR_model =="sigma":
+#             self.fid_Ms = jsm_SHMR.sigma([self.data.truths[0], 0, self.data.truths[2],  self.data.truths[3], 0], self.halo_masses)
+#             for i,val in enumerate(self.last_samp):  # now pushing all thetas through!
+#                 SHMR_mat[i] = jsm_SHMR.sigma([val[0], 0, val[2], val[3], 0], self.halo_masses)
+
+#         elif self.SHMR_model =="redshift":
+#             self.fid_Ms = jsm_SHMR.redshift([self.data.truths[0], 0, self.data.truths[2],  self.data.truths[3], 0, self.data.truths[5]], self.halo_masses, np.zeros(shape=self.halo_masses.shape[0]))
+#             for i,val in enumerate(self.last_samp):  # now pushing all thetas through!
+#                 SHMR_mat[i] = jsm_SHMR.redshift([val[0], 0, val[2], val[3], 0, val[5]], self.halo_masses, np.zeros(shape=self.halo_masses.shape[0]))
+
+#         else:
+#             print("no model selected")
+                
+#         plt.figure(figsize=(10, 8))
+#         for i,val in enumerate(SHMR_mat):
+#             plt.plot(self.halo_masses, val, alpha=0.3, lw=1, color='grey')
+#         plt.plot(self.halo_masses, self.fid_Ms, color="orange", label=str(self.data.truths), lw=3)
+
+#         plt.axhline(self.min_mass, label="mass limit", lw=1, ls=":", color="black")
+#         plt.scatter(self.data.lgMh_flat, self.data.lgMs_flat, marker=".", color="black")
+#         plt.ylim(4,11)
+#         plt.xlim(7.5,12)
+#         plt.ylabel("M$_{*}$ (M$_\odot$)", fontsize=15)
+#         plt.xlabel("M$_{\mathrm{vir}}$ (M$_\odot$)", fontsize=15)
+#         plt.legend(fontsize=12)
+#         plt.savefig(self.savedir+"SHMR.png")
 
     # def sigma_plot(self):
     #     sigma = np.zeros(shape=(self.last_samp.shape[0],self.data.lgMh_flat.shape[0]))
@@ -333,6 +552,8 @@ class inspect_run:
         #     cmap = mpl.cm.ScalarMappable(norm=norm, cmap=mpl.cm.magma_r)
         #     colors = mpl.cm.magma_r(np.linspace(0, 1, len(self.last_chisq)))
         #     clabel = "$\\chi^2$"
+
+
 
 class analyze_chains:
 
