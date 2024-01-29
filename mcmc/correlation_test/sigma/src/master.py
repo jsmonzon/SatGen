a623f7ef9a351c0f6eeea36dc0c8943a76d13a2c
Sebastian Monzon <jsmonzon@ucsc.edu>
2024-01-29 11:26:17 -0500

    running sigma test

---
+++ mcmc/correlation_test/sigma/src/master.py
@@ -0,0 +1,29 @@
+import subprocess
+import json
+
+# Define your global variables here
+config = {
+    "location": "server",
+    "a_stretch": 2.0,
+    "nwalk": 100,
+    "nstep": 3000,
+    "ncores": 16,
+    "min_mass": 6.5,
+    "Ntree": 100,
+    "N_corr": True,
+    "p0_corr": True,
+    "savefig": True,
+    "reset": True,
+}
+
+# Write the configuration to a JSON file
+with open("config.json", "w") as f:
+    json.dump(config, f)
+
+mock_scripts = ["mock_1.py", "mock_2.py", "mock_3.py", "mock_4.py"]
+
+for idx, mock_script in enumerate(mock_scripts, start=1):
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+    print(f"running mock {idx}")
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+    subprocess.run(["python", mock_script])
