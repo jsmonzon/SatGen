7b2df7913e3281ca4da05fff514e35d4c0e160b9
Sebastian Monzon <jsmonzon@ucsc.edu>
2024-09-04 15:49:37 -0400

    to run new convergence test

---
+++ mcmc/inference_tests/convergence/mock_2/src/master.py
@@ -0,0 +1,36 @@
+import subprocess
+import json
+
+mock_dirs = ["../Nhost_100/", "../Nhost_1000/", "../Nhost_10000/"]
+
+config = {
+    "location": "local",
+    "a_stretch": 1.2,
+    "nwalk": 15,
+    "nstep": 100,
+    "ncores": 16,
+    "min_mass": 6.5,
+    "max_mass":11.5,
+    "Nsamp": 1,
+    "max_N": 1000,
+    "init_gauss": 1e-4,
+    "N_corr": True,
+    "p0_corr": True,
+    "savefig": True,
+    "reset": True,
+    }
+
+for idx, dir in enumerate(mock_dirs, start=0):
+
+    config["savedir"] = dir
+    config["start_theta"] = [10.5, 2.0, 0, 0, 0.2, 0]
+
+    # Write the configuration to a JSON file
+    with open("config.json", "w") as f:
+        json.dump(config, f)
+
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+    print(f"running chain in {dir}")
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+
+    subprocess.run(["python", "chain_runner.py"])
--- mcmc/src/jsm_models.py
+++ mcmc/src/jsm_models.py
@@ -5,113 +5,193 @@ import warnings; warnings.simplefilter('ignore')
 import jsm_SHMR
 
 
-class SAMPLE_SAGA_MODELS:
+class SAMPLE_SAGA_NEW:
 
-    def __init__(self, fid_theta:list, meta_path:str, SAGA_ind:int, Nreal_per_model=None, redshift_depandance=False, savedir=None, Nsigma_samples=1):
+    def __init__(self, fid_theta:list, meta_path:str, SAGA_ind:int, Nhost_M=None, savedir=None, savemodel=None, plot=False):
         self.fid_theta = fid_theta
         self.meta_path = meta_path
+        self.SAGA_ind = SAGA_ind
+        self.Nhost_M = Nhost_M
         self.savedir = savedir
-        self.Nsigma_samples = Nsigma_samples
+        self.savemodel = savemodel
+        self.plot = plot
         
         models = np.load(self.meta_path+"models.npz") # already broken up into SAGA samples!
 
-        if SAGA_ind < 100:
-            print("selecting the", str(SAGA_ind), " SAGA sample")
-            self.lgMh_data = models["mass"][SAGA_ind] 
-            self.zacc_data = models["redshift"][SAGA_ind]
+        print("selecting the", str(self.SAGA_ind), " SAGA sample")
+        self.lgMh_data = models["mass"][self.SAGA_ind] 
 
-            print("converting the subhalos to satellites and creating the mock data instance")
-            if redshift_depandance == True:
-                self.lgMs_data = jsm_SHMR.general_new(fid_theta, self.lgMh_data, self.zacc_data, self.Nsigma_samples)
-            else:
-                self.lgMs_data = jsm_SHMR.general_new(fid_theta, self.lgMh_data, 0, self.Nsigma_samples)
-            if savedir != None:
-                print("saving the mock data")
-                np.savez(self.savedir+"mock_data.npz",
-                        halo_mass = self.lgMh_data,
-                        stellar_mass = self.lgMs_data,
-                        zacc = self.zacc_data,
-                        fid_theta = self.fid_theta)
-        
-            self.lgMh_data_flat = self.lgMh_data.flatten()
-            self.lgMs_data_flat = self.lgMs_data.flatten()
-            # plt.figure(figsize=(8, 8))
-            # plt.title("$\\theta_{\mathrm{fid}}$ = "+f"{self.fid_theta}")
-            # plt.scatter(self.lgMh_data_flat, self.lgMs_data_flat, marker="*", color="black")
-            # plt.ylabel("M$_{*}$ (M$_\odot$)", fontsize=15)
-            # plt.xlabel("M$_{\mathrm{vir}}$ (M$_\odot$)", fontsize=15)
-            # plt.xlim(8.5, 12)
-            # plt.ylim(6.0, 10.5)
-            # if savedir != None:
-            #     plt.savefig(self.savedir+"mock_SHMR.pdf")
-            # plt.show()
-
-            if Nreal_per_model != None:
-                self.Nreal_per_model = Nreal_per_model
-                self.lgMh_models = np.delete(np.vstack(models["mass"]), np.arange(100), axis=0)
-                self.N_model_realizations = int(self.lgMh_models.shape[0] / self.Nreal_per_model)
-                print(f"there are {self.N_model_realizations} unique model realizations, each made up of {self.Nreal_per_model} merger trees")
-                self.Nreal_extra = self.lgMh_models.shape[0] % self.Nreal_per_model
-                print(f"there are {self.Nreal_extra} extra merger trees, deleting these unused trees")
-                if self.Nreal_extra == 0:
-                    self.lgMh_models = self.lgMh_models.reshape([self.N_model_realizations, self.Nreal_per_model, self.lgMh_models.shape[1]])
-                else:
-                    self.lgMh_models = np.delete(self.lgMh_models, np.arange(self.Nreal_extra), axis=0) 
-                    self.lgMh_models = self.lgMh_models.reshape([self.N_model_realizations, self.Nreal_per_model, self.lgMh_models.shape[1]])
-
-            else:
-                print("breaking off the remaining samples and creating the model instance")    
-                self.lgMh_models = np.vstack(np.delete(models["mass"], SAGA_ind, axis=0))
-                self.zacc_models = np.vstack(np.delete(models["redshift"], SAGA_ind, axis=0))
-
-            if savedir != None:
-                print("saving the models")
-                np.savez_compressed(self.savedir+"remaining_models.npz",
-                        halo_mass = self.lgMh_models,
-                        zacc = self.zacc_models)
+        print("converting the subhalos to satellites and creating the mock data instance")
+        self.lgMs_data = jsm_SHMR.general_new(fid_theta, self.lgMh_data, 0, 1)
+        if self.savedir != None:
+            print("saving the mock data")
+            np.savez(self.savedir+"mock_data.npz",
+                    halo_mass = self.lgMh_data,
+                    stellar_mass = self.lgMs_data,
+                    fid_theta = self.fid_theta,
+                    SAGA_ind = self.SAGA_ind)
             
-        elif SAGA_ind > 100:
-            N_SAGA = int(SAGA_ind/100)
-            print("selecting the first", N_SAGA, "SAGA samples")
-            self.lgMh_data = np.vstack(models["mass"][0:N_SAGA]) 
-            self.zacc_data = np.vstack(models["redshift"][0:N_SAGA])
+        print("breaking off the remaining samples and creating the model instance")
+        if self.Nhost_M != None:
+            print("Only using the first", str(self.Nhost_M), "trees for the model")
+            self.lgMh_models = np.vstack(np.delete(models["mass"], self.SAGA_ind, axis=0))[0:self.Nhost_M]
+            if self.savemodel != None:
+                print("saving the models")
+                np.savez_compressed(self.savemodel+"remaining_models.npz",
+                        halo_mass = self.lgMh_models,
+                        Nhost_M = self.Nhost_M)    
 
-            print("converting the subhalos to satellites and creating the mock data instance")
-            if redshift_depandance == True:
-                self.lgMs_data = jsm_SHMR.general_new(fid_theta, self.lgMh_data, self.zacc_data, self.Nsigma_samples)
-            else:
-                self.lgMs_data = jsm_SHMR.general_new(fid_theta, self.lgMh_data, 0, self.Nsigma_samples)
-
-            if savedir != None:
-                print("saving the mock data")
-                np.savez(self.savedir+"mock_data.npz",
-                        halo_mass = self.lgMh_data,
-                        stellar_mass = self.lgMs_data,
-                        zacc = self.zacc_data,
-                        fid_theta = self.fid_theta)
-        
+        else:
+            self.lgMh_models = np.vstack(np.delete(models["mass"], self.SAGA_ind, axis=0))
+            if self.savemodel != None:
+                print("saving the models")
+                np.savez_compressed(self.savemodel+"remaining_models.npz",
+                        halo_mass = self.lgMh_models,
+                        Nhost_M = 10000)    
+                    
+        if self.plot:
             self.lgMh_data_flat = self.lgMh_data.flatten()
             self.lgMs_data_flat = self.lgMs_data.flatten()
-            # plt.figure(figsize=(8, 8))
-            # plt.title("$\\theta_{\mathrm{fid}}$ = "+f"{self.fid_theta}")
-            # plt.scatter(self.lgMh_data_flat, self.lgMs_data_flat, marker="*", color="black")
-            # plt.ylabel("M$_{*}$ (M$_\odot$)", fontsize=15)
-            # plt.xlabel("M$_{\mathrm{vir}}$ (M$_\odot$)", fontsize=15)
-            # plt.xlim(8.5, 12)
-            # plt.ylim(6.0, 10.5)
-            # if savedir != None:
-            #     plt.savefig(self.savedir+"mock_SHMR.pdf")
-            # plt.show()
+            plt.figure(figsize=(8, 8))
+            plt.title("$\\theta_{\mathrm{fid}}$ = "+f"{self.fid_theta}")
+            plt.scatter(self.lgMh_data_flat, self.lgMs_data_flat, marker="*", color="black")
+            plt.ylabel("M$_{*}$ (M$_\odot$)", fontsize=15)
+            plt.xlabel("M$_{\mathrm{vir}}$ (M$_\odot$)", fontsize=15)
+            plt.xlim(8.5, 12)
+            plt.ylim(6.0, 10.5)
+            if self.savedir != None:
+                plt.savefig(self.savedir+"mock_SHMR.pdf")
+            plt.show()
 
-            print("breaking off the remaining samples and creating the model instance")    
-            self.lgMh_models = np.vstack(np.delete(models["mass"], np.arange(N_SAGA), axis=0))
-            self.zacc_models = np.vstack(np.delete(models["redshift"], np.arange(N_SAGA), axis=0))
 
-            if savedir != None:
-                print("saving the models")
-                np.savez_compressed(self.savedir+"remaining_models.npz",
-                        halo_mass = self.lgMh_models,
-                        zacc = self.zacc_models)
+class SAMPLE_SAGA_MODELS:
+
+    def __init__(self, fid_theta: list, meta_path: str, extra_path: str, SAGA_ind: int, reshape=None, verbose=True):
+        self.fid_theta = fid_theta
+        self.meta_path = meta_path
+        self.extra_path = extra_path
+        self.verbose = verbose
+        
+        if extra_path is not None:
+            models = np.load(self.meta_path+"models.npz")  # already broken up into SAGA samples!
+            models_extra = np.load(self.extra_path+"models.npz")  # already broken up into SAGA samples!
+
+        if reshape is not None:
+            if self.verbose:
+                print("reshaping the array, it should still be 3D!!")
+            self.lgMh = models["mass"].reshape([reshape[0], reshape[1], models["mass"].shape[2]])
+            self.lgMh_extra = models_extra["mass"].reshape([reshape[0], reshape[1], models_extra["mass"].shape[2]]) 
+        else:
+            self.lgMh = models["mass"]
+            self.lgMh_extra = models_extra["mass"]
+
+        if self.verbose:
+            print("selecting the", str(SAGA_ind), " SAGA sample")
+        self.lgMh_data = self.lgMh[SAGA_ind]
+
+        if self.verbose:
+            print("Grabbing the extra trees to make the model and collapsing to 2D!")
+        self.lgMh_model = np.vstack(self.lgMh_extra)
+
+        if self.verbose:
+            print("converting the subhalos to satellites")
+        self.lgMs_data = jsm_SHMR.general_new(fid_theta, self.lgMh_data, 0, 1)
+        self.lgMs_model = jsm_SHMR.general_new(fid_theta, self.lgMh_model, 0, 1)
+
+
+class SAMPLE_CROSS:
+
+    def __init__(self, fid_theta: list, meta_path: str, SAGA_ind: int, reshape=None, verbose=True):
+        self.fid_theta = fid_theta
+        self.meta_path = meta_path
+        self.verbose = verbose
+        
+        models = np.load(self.meta_path+"models.npz")  # already broken up into SAGA samples!
+
+        if reshape is not None:
+            if self.verbose:
+                print("reshaping the array, it should still be 3D!!")
+            self.lgMh = models["mass"].reshape([reshape[0], reshape[1], models["mass"].shape[2]])
+        else:
+            self.lgMh = models["mass"]
+
+        if self.verbose:
+            print("selecting the", str(SAGA_ind), " SAGA sample")
+        self.lgMh_data = self.lgMh[SAGA_ind]
+
+        if self.verbose:
+            print("Grabbing the remaining trees to make the model and collapsing to 2D!")
+        self.lgMh_model = np.vstack(np.delete(self.lgMh, SAGA_ind, axis=0))
+
+        if self.verbose:
+            print("converting the subhalos to satellites")
+        self.lgMs_data = jsm_SHMR.general_new(fid_theta, self.lgMh_data, 0, 1)
+        self.lgMs_model = jsm_SHMR.general_new(fid_theta, self.lgMh_model, 0, 1)
+
+        # self.Nreal_per_model = Nreal_per_model
+
+        # #print(f"there are {self.N_model_realizations} unique model realizations, each made up of {self.Nreal_per_model} merger trees")
+        # self.Nreal_extra = self.lgMh_models.shape[0] % self.Nreal_per_model
+        #     #print(f"there are {self.Nreal_extra} extra merger trees, deleting these unused trees")
+        #     if self.Nreal_extra == 0:
+        #         self.lgMh_models = self.lgMh_models.reshape([self.N_model_realizations, self.Nreal_per_model, self.lgMh_models.shape[1]])
+        #     else:
+        #         self.lgMh_models = np.delete(self.lgMh_models, np.arange(self.Nreal_extra), axis=0) 
+        #         self.lgMh_models = self.lgMh_models.reshape([self.N_model_realizations, self.Nreal_per_model, self.lgMh_models.shape[1]])
+
+        #     else:
+        #         print("breaking off the remaining samples and creating the model instance")    
+        #         self.lgMh_models = np.vstack(np.delete(models["mass"], SAGA_ind, axis=0))
+        #         self.zacc_models = np.vstack(np.delete(models["redshift"], SAGA_ind, axis=0))
+
+        #     if savedir != None:
+        #         print("saving the models")
+        #         np.savez_compressed(self.savedir+"remaining_models.npz",
+        #                 halo_mass = self.lgMh_models,
+        #                 zacc = self.zacc_models)
+            
+        # elif SAGA_ind > 100:
+        #     N_SAGA = int(SAGA_ind/100)
+        #     print("selecting the first", N_SAGA, "SAGA samples")
+        #     self.lgMh_data = np.vstack(models["mass"][0:N_SAGA]) 
+        #     self.zacc_data = np.vstack(models["redshift"][0:N_SAGA])
+
+        #     print("converting the subhalos to satellites and creating the mock data instance")
+        #     if redshift_depandance == True:
+        #         self.lgMs_data = jsm_SHMR.general_new(fid_theta, self.lgMh_data, self.zacc_data, self.Nsigma_samples)
+        #     else:
+        #         self.lgMs_data = jsm_SHMR.general_new(fid_theta, self.lgMh_data, 0, self.Nsigma_samples)
+
+        #     if savedir != None:
+        #         print("saving the mock data")
+        #         np.savez(self.savedir+"mock_data.npz",
+        #                 halo_mass = self.lgMh_data,
+        #                 stellar_mass = self.lgMs_data,
+        #                 zacc = self.zacc_data,
+        #                 fid_theta = self.fid_theta)
+        
+        #     self.lgMh_data_flat = self.lgMh_data.flatten()
+        #     self.lgMs_data_flat = self.lgMs_data.flatten()
+        #     # plt.figure(figsize=(8, 8))
+        #     # plt.title("$\\theta_{\mathrm{fid}}$ = "+f"{self.fid_theta}")
+        #     # plt.scatter(self.lgMh_data_flat, self.lgMs_data_flat, marker="*", color="black")
+        #     # plt.ylabel("M$_{*}$ (M$_\odot$)", fontsize=15)
+        #     # plt.xlabel("M$_{\mathrm{vir}}$ (M$_\odot$)", fontsize=15)
+        #     # plt.xlim(8.5, 12)
+        #     # plt.ylim(6.0, 10.5)
+        #     # if savedir != None:
+        #     #     plt.savefig(self.savedir+"mock_SHMR.pdf")
+        #     # plt.show()
+
+        #     print("breaking off the remaining samples and creating the model instance")    
+        #     self.lgMh_models = np.vstack(np.delete(models["mass"], np.arange(N_SAGA), axis=0))
+        #     self.zacc_models = np.vstack(np.delete(models["redshift"], np.arange(N_SAGA), axis=0))
+
+        #     if savedir != None:
+        #         print("saving the models")
+        #         np.savez_compressed(self.savedir+"remaining_models.npz",
+        #                 halo_mass = self.lgMh_models,
+        #                 zacc = self.zacc_models)
 
 class LOAD_DATA:
 
@@ -138,7 +218,10 @@ class LOAD_MODELS:
         self.mfile = mfile
         models = np.load(mfile)
         self.lgMh_models = models["halo_mass"]
-        self.zacc_models = models["zacc"]
+        try:
+            self.zacc_models = models["zacc"]
+        except KeyError:
+            print("no redshift info supplied")
 
     def get_stats(self, theta:list, min_mass:float, max_N:float, Nsigma_samples=1):
         self.theta = theta
